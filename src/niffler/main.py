ed72f2bfeb95e44dd67df01d46bd1a0f55e9bf20
宋秉徽 <451000936@qq.com>
2024-12-17 14:08:25 +0800

    feat: tg bot

--- src/niffler/main.py
+++ src/niffler/main.py
@@ -1,11 +1,24 @@
-from fastapi import FastAPI
+import httpx
+from fastapi import FastAPI, HTTPException
+
+from niffler.config import get_settings
 
 app = FastAPI()
 
 
 @app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+async def get_bot_info():
+    url = f"https://api.telegram.org/bot{get_settings().tg_bot_token}/getMe"
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(url)
+            response.raise_for_status()
+            return response.json()
+        except httpx.HTTPStatusError as e:
+            raise HTTPException(status_code=e.response.status_code, detail=str(e))
+        except httpx.RequestError as e:
+            raise HTTPException(status_code=500, detail=f"Request failed: {str(e)}")
 
 
 if __name__ == "__main__":
