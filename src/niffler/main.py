10eb152aa0e08310eea34a9c0b597c5440b0fc5b
宋秉徽 <451000936@qq.com>
2025-03-20 18:28:14 +0800

    feat: tg 机器人配置

--- src/niffler/main.py
+++ src/niffler/main.py
@@ -1,15 +1,12 @@
-# from telegram.ext import CommandHandler
-# from niffler.bot import Bot
-# from niffler.handlers import start
-# bot = Bot()
-# bot.add_handler(CommandHandler("start", start.handler))
-# bot.run_polling()
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from telegram.ext import CommandHandler
 
+from niffler.bot import Bot
 from niffler.config.db import DBManager
 from niffler.config.scheduler import SchedulerManager
+from niffler.handlers import start
 from niffler.routers import coin
 from niffler.tasks.coin_hunter import CoinHunter
 
@@ -18,9 +15,13 @@ app = FastAPI()
 db = DBManager()
 scheduler = SchedulerManager()
 
+bot = Bot()
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    bot.add_handler(CommandHandler("start", start.handler))
+    bot.run_polling()
     await db.connect()
     scheduler.add_task(CoinHunter())
     await scheduler.start()
